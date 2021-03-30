42bbceca7b74e74b6de6598f6ee6c27822424532
Nuolong <nmcshea@nd.edu>
2021-03-30 01:58:23 -0400

    todos

--- window.py
+++ window.py
@@ -2,10 +2,9 @@
 
 # imports
 import tkinter as tk
+import train
 
 # constants
-FILE_COUNT = 5
-actions = ["Encrypt", "Decrypt", "Merge", "Slice"]
 background_clr = "#e6e6ff"
 
 # window settings
@@ -13,8 +12,17 @@ window = tk.Tk()
 window.title('Pestlexis')
 window.config(background = background_clr)
 
+def need_data_page():
+    print("In need_data_page()")
+    # TODO: show welcome, need data.json
+
 def new_user_page():
     print("In new_user_page()")
+    # TODO: show welcome page, define json fields from data.json and create progress.json
+
+def default_page():
+    print("In default_page()")
+    # TODO: do program
 
 # wait for events
 window.mainloop()
